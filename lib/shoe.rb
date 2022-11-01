fc85fc595524d6ffc305968c4f00ee1b4988ec28
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-01 11:19:43 -0400

    commit tree d0ce258873a41c31e139e59cfd8effb5496370cb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_reader :brand
+   attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+nikes = Shoe.new("Nike")
